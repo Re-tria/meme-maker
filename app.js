18c51efdd8c51a61b0ef9acb7a64502d5abea407
Re-tria <52969866+Re-tria@users.noreply.github.com>
2023-01-15 21:24:47 +0900

    Add Color

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("line-width");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const color = document.getElementById("color");
 
 // Default Setting
 canvas.width = 800;
@@ -10,17 +14,28 @@ ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 const colors = [
   "#1abc9c",
+  "#16a085",
   "#2ecc71",
+  "#27ae60",
   "#3498db",
+  "#2980b9",
   "#9b59b6",
+  "#8e44ad",
   "#34495e",
+  "#2c3e50",
   "#f1c40f",
+  "#f39c12",
   "#e67e22",
+  "#d35400",
   "#e74c3c",
+  "#c0392b",
   "#ecf0f1",
+  "#bdc3c7",
   "#95a5a6",
+  "#7f8c8d",
 ];
 
+// Draw Line Function
 function onMove(event) {
   if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
@@ -39,15 +54,36 @@ function cancelPainting() {
   ctx.beginPath();
 }
 
+// Setting Line-Width Function
 function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
 
-// Draw Line
+// Setting Color-Options Function
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+function onColorChange() {
+  const colorValue = color.value;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+}
+
+// Draw Line Event
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
-// Setting Line-Width
+// Setting Line-Width Event
 lineWidth.addEventListener("change", onLineWidthChange);
+
+// Setting Color-Options Event
+colorOptions.forEach((color) => {
+  color.addEventListener("click", onColorClick);
+});
+color.addEventListener("change", onColorChange);
